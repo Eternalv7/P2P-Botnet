a0696eb318798a3e6bba903c46c49c8e5142718a
Hacker5preme <hacker5preme@protonmail.com>
2021-06-18 16:29:59 +0200

    Updated Structure and Ideas became more concret

--- Setup/Initialize.py
+++ Setup/Initialize.py
@@ -1,1 +1,42 @@
-Init
+# Author: Ron Jost (Hacker5preme)
+# Version 0.1
+# Only for educational purposes ;)
+
+'''
+Descritpion:
+This program should initialize the botnet. 
+It only needs to run once at the start!
+What does it do?
+It setups the infrastructure for the Botnet 
+Requirements:
+    - Bot A 
+    - Independet Webserver
+'''
+
+
+'''
+Banner:
+'''
+banner = """
+
+ ____ ____  ____       ____        _              _   
+|  _ \___ \|  _ \     | __ )  ___ | |_ _ __   ___| |_ 
+| |_) |__) | |_) |____|  _ \ / _ \| __| '_ \ / _ \ __|
+|  __// __/|  __/_____| |_) | (_) | |_| | | |  __/ |_ 
+|_|  |_____|_|        |____/ \___/ \__|_| |_|\___|\__|
+                                                      
+                                    @Hacker5preme 
+                        
+
+
+"""
+print(banner)
+print('[*] Initializing Botnet')
+
+
+'''
+Import required modules:
+'''
+
+
+
